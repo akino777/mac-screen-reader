b41eb40238616ad9d631f7b477e47adf710e8d2d
akino777 <terashima.akinobu@nifty.co.jp>
2026-02-09 18:08:24 +0900

    Fix Mint build compatibility with macOS 15 SDK
    - Add swift settings to disable availability checking for backward compatibility
    - Allows CGWindowListCreateImage to be used despite deprecation in macOS 15
    - Maintains support for macOS 10.15-14.x while building on newer SDKs

--- Package.swift
+++ Package.swift
@@ -18,6 +18,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "MacScreenReader")
+            name: "MacScreenReader",
+            swiftSettings: [
+                // Allow use of APIs that are deprecated in newer macOS versions
+                // but required for compatibility with macOS 10.15-14.x
+                .unsafeFlags(["-Xfrontend", "-disable-availability-checking"])
+            ]
+        )
     ]
 )
